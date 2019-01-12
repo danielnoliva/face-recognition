065eaff6da8016955b6dc144270cd44919c70c11
Daniel Oliva <olivaf.daniel@gmail.com>
2019-01-11 18:17:52 -0700

    ImageLinkForm added with some styling

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./ImageLinkForm.css"
 
 function ImageLinkForm() {
   return (
     <div>
-      
+      <p className="f3 white">
+        {"The target seeker will detect the faces in your pictures."}
+      </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            name="file"
+            id="file"
+          />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-navy">
+            Detect
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ImageLinkForm
+export default ImageLinkForm;
